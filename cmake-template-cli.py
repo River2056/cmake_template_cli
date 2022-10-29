45841cea248feab14a8e3573d12f49e461a7d051
Kevin.Tung <chen0625tung@gmail.com>
2022-10-29 20:58:26 +0800

    + refactor to use function for better readability

--- cmake-template-cli.py
+++ cmake-template-cli.py
@@ -6,6 +6,23 @@ import subprocess
 
 from pathlib import Path
 
+
+def write_to_output(path, content):
+    with open(path, "wt", encoding="utf-8") as output:
+        output.write(textwrap.dedent(content))
+
+
+def create_nested_file(path, content):
+    path.touch()
+    print(f"{str(path)} created")
+    write_to_output(path, content)
+    print(f"Done creating {path}")
+
+
+def test():
+    pass
+
+
 def main():
     parser = argparse.ArgumentParser(
         description="cmake template cli tool",
@@ -49,51 +66,40 @@ def main():
     external_dir_path.mkdir()
     print(f"{str(external_dir_path)} created")
 
-    # Makefile
-    makefile_path = Path(project_dir_path.joinpath("Makefile"))
-    makefile_path.touch()
-    print(f"{str(makefile_path)} created")
-    with open(makefile_path, "wt", encoding="utf-8") as output:
-        output.write(
-            textwrap.dedent(
-                """
-                exe_name := task
+    # root Makefile
+    create_nested_file(
+        Path(project_dir_path.joinpath("Makefile")),
+        """
+            exe_name := task
 
-                all: run
+            all: run
 
-                clean:
-                \trm -rf ./build
-                \tmkdir build
+            clean:
+            \trm -rf ./build
+            \tmkdir build
 
-                build: clean
-                \tcmake -S . -B ./build
-                \tcmake --build ./build
+            build: clean
+            \tcmake -S . -B ./build
+            \tcmake --build ./build
 
-                run: build
-                \t./build/app/Debug/$(exe_name).exe
+            run: build
+            \t./build/app/Debug/$(exe_name).exe
 
-                pre_generate: clean
-                \tcmake -S . -B ./build -GNinja
-                \tcmake --build ./build
-                \tmv ./build/compile_commands.json .
-                
-                generate: pre_generate
-                \trm -rf ./build
-                \tmkdir build
-            """
-            )
-        )
-    print("Done creating Makefile")
-    # Makefile end
+            pre_generate: clean
+            \tcmake -S . -B ./build -GNinja
+            \tcmake --build ./build
+            \tmv ./build/compile_commands.json .
+            
+            generate: pre_generate
+            \trm -rf ./build
+            \tmkdir build
+        """,
+    )
 
     # root CMakeLists.txt
-    root_cmakelists_path = Path(project_dir_path).joinpath("CMakeLists.txt")
-    root_cmakelists_path.touch()
-    print(f"{str(root_cmakelists_path)} created")
-    with open(root_cmakelists_path, "wt", encoding="utf-8") as output:
-        output.write(
-            textwrap.dedent(
-                """
+    create_nested_file(
+        Path(project_dir_path).joinpath("CMakeLists.txt"),
+        """
             cmake_minimum_required(VERSION 3.16)
 
             project(task)
@@ -108,22 +114,13 @@ def main():
 
             add_subdirectory(src)
             add_subdirectory(app)
-        """
-            )
-        )
-    print("Done creating root CMakeLists.txt")
-    # root CMakeLists.txt end
+        """,
+    )
 
     # AddGitSubmodule.cmake
-    add_git_submodule_cmake_path = Path(cmake_dir_path).joinpath(
-        "AddGitSubmodule.cmake"
-    )
-    add_git_submodule_cmake_path.touch()
-    print(f"{str(add_git_submodule_cmake_path)} created")
-    with open(add_git_submodule_cmake_path, "wt", encoding="utf-8") as output:
-        output.write(
-            textwrap.dedent(
-                """
+    create_nested_file(
+        Path(cmake_dir_path).joinpath("AddGitSubmodule.cmake"),
+        """
             function(add_git_submodule dir)
             \tfind_package(Git REQUIRED)
 
@@ -135,70 +132,48 @@ def main():
 
             \tadd_subdirectory(${dir})
             endfunction() 
-        """
-            )
-        )
-    print("Done creating AddGitSubmodule.cmake")
-    # AddGitSubmodule.cmake end
+        """,
+    )
 
     # app/main.cpp
-    main_cpp_path = Path(app_dir_path).joinpath("main.cpp")
-    main_cpp_path.touch()
-    print(f"{str(main_cpp_path)} created")
-    with open(main_cpp_path, "wt", encoding="utf-8") as output:
-        output.write(
-            textwrap.dedent(
-                """
+    create_nested_file(
+        Path(app_dir_path).joinpath("main.cpp"),
+        """
             #include <iostream>
 
             int main() {
                 std::cout << "hello from cmake template cli!" << '\\n';
                 return 0;
             }
-        """
-            )
-        )
-    print("Done creating app/main.cpp")
-    # app/main.cpp end
+        """,
+    )
 
     # app/CMakeLists.txt
-    app_cmakelists_txt_path = Path(app_dir_path).joinpath("CMakeLists.txt")
-    app_cmakelists_txt_path.touch()
-    print(f"{str(app_cmakelists_txt_path)} created")
-    with open(app_cmakelists_txt_path, "wt", encoding="utf-8") as output:
-        output.write(
-            textwrap.dedent(
-                """
+    create_nested_file(
+        Path(app_dir_path).joinpath("CMakeLists.txt"),
+        """
             set(EXECUTABLE_SOURCES 
                 "main.cpp")
 
             add_executable(${PROJECT_NAME} ${EXECUTABLE_SOURCES})
-        """
-            )
-        )
-    print("Done creating app/CMakeLists.txt")
-    # app/CMakeLists.txt end
+        """,
+    )
 
     # src/CMakeLists.txt
+    # empty CMakeLists.txt, add custom later
     Path(src_dir_path).joinpath("CMakeLists.txt").touch()
     print("Done creating src/CMakeLists.txt")
-    # src/CMakeLists.txt end
 
     # .gitignore
-    gitignore_path = Path(project_dir_path).joinpath(".gitignore")
-    gitignore_path.touch()
-    with open(gitignore_path, "wt", encoding="utf-8") as output:
-        output.write(
-            textwrap.dedent(
-                """
+    create_nested_file(
+        Path(project_dir_path).joinpath(".gitignore"),
+        """
             .ccls-cache/
             external/
             build/
             compile_commands.json
-        """
-            )
-        )
-    # gitignore end
+        """,
+    )
 
     # git init
     os.chdir(project_dir_path)
